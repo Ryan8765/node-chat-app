56ad9ca43a8c377eabd7841a51c4a24a94428c60
Ryan Haas <Ryan Haas>
2016-12-23 11:34:27 -0600

    Updated to support socket.io server and communicate between client and server

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ _______________________________________________
 1. Path 
 	- documentation - https://nodejs.org/api/path.html
 	- Using this to set up connecting the server to the html files by using the path.join method.  This allows us to get access to the public folder from the server.js file without the URI going into the server and then back out of the server file. 
+
+2. Express
+	- documenation - http://expressjs.com/
+
+3. Socket IO
+	- makes it easy to set up a server that allows communication from front end to back end via the server.
+	- You get access to a route that accepts incoming connections and we got access to a javascript library. localhost:3000/socket.io.js
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-const path = require('path');
-const express = require('express');
+const path     = require('path');
+const http     = require('http');
+const express  = require('express');
+const socketIO = require('socket.io');
 
 //use publicPath to get to the public folder.  using "path" module allows you to get there without the file structure going into the server fold and then back out again.
 const publicPath = path.join(__dirname, '../public');
@@ -8,11 +10,25 @@ const port = process.env.PORT || 3000;
 
 //user express npm module for this application
 var app = express();
+//this is used to set up bi-communication for socket.io
+var server = http.createServer( app );
+//configure the server to use socket.io. This is how we communicate between the server and the client.
+var io = socketIO( server );
 
 
 //this gets express to set the path to where the html files will be served up from
 app.use(express.static(publicPath));
 
-app.listen(3000,  () => {
+io.on('connection',  (socket) => {
+	console.log('New user connected!!!!!');
+	//register to the cmd when the user disconnects
+	socket.on('disconnect',  () => {
+		console.log( "Client disconnected" );
+	});
+});
+
+
+//we are using server.listen because we are using socket.io
+server.listen(3000,  () => {
 	console.log(`Server is running on port ${port}`);
 });
